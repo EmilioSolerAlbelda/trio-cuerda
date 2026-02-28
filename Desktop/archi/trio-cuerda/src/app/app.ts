c5f699a43befd864de20777aa601f40f810d77fb
EmilioSolerAlbelda <emiliosolerdeveloper@gmail.com>
2026-02-28 16:22:57 +0100

    home principal y footer

--- Desktop/archi/trio-cuerda/src/app/app.ts
+++ Desktop/archi/trio-cuerda/src/app/app.ts
@@ -1,11 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
-
 import { NavbarComponent } from './shared/navbar/navbar';
-
-
 import { HomeComponent } from './pages/home/home';
 
 @Component({
@@ -15,6 +11,19 @@ import { HomeComponent } from './pages/home/home';
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Trio de Cuerda';
+  mostrarCookies: boolean = true;
+
+  ngOnInit() {
+    const aceptadas = localStorage.getItem('cookies_aceptadas');
+    if (aceptadas === 'true') {
+      this.mostrarCookies = false;
+    }
+  }
+
+  aceptarCookies() {
+    localStorage.setItem('cookies_aceptadas', 'true');
+    this.mostrarCookies = false;
+  }
 }
--- Desktop/archi/trio-cuerda/src/app/pages/home/home.ts
+++ Desktop/archi/trio-cuerda/src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,5 +8,30 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.html', 
   styleUrls: ['./home.css']   
 })
-export class HomeComponent { 
+export class HomeComponent implements AfterViewInit, OnDestroy { 
+  
+  @ViewChild('miVideo') videoElement!: ElementRef<HTMLVideoElement>;
+  private observer!: IntersectionObserver;
+
+  ngAfterViewInit() {
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.videoElement.nativeElement.play().catch(() => {});
+        } else {
+          this.videoElement.nativeElement.pause();
+        }
+      });
+    }, { threshold: 0.5 });
+
+    if (this.videoElement) {
+      this.observer.observe(this.videoElement.nativeElement);
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
 }
